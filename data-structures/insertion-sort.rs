77ab45cfd1d5b65061d596ea7924a5b7ccbdbf9d
Damien Schoof <damien.schoof@gmail.com>
2013-05-29 22:17:25 -0400

    Add insertion sort implementation.

---
+++ data-structures/insertion-sort.rs
@@ -0,0 +1,32 @@
+/* Sample insertion sort program in Rust.
+   Tested to compile with rust-0.6.
+ */
+extern mod extra;
+extern mod benchmark;
+use std::vec;
+use benchmark::Benchmark;
+
+fn insertion_sort<T:Ord+Copy>(arr: ~[T]) -> ~[T] {
+    let mut index = 0;
+    let length = arr.len();
+    let mut result = copy arr;
+
+    while index < length {
+        let valueToInsert = result[index];
+        let mut holePos = index;
+
+        while holePos > 0 && valueToInsert < result[holePos - 1] {
+            vec::swap(result, holePos, holePos - 1);
+            holePos -= 1;
+        }
+        index += 1;
+    }
+
+    return result
+}
+
+fn main() {
+    let mut bench = Benchmark::new();
+    bench.run(insertion_sort);
+}
+
