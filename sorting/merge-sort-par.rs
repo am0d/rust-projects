2aa6f0fec295636b3883da7c27ed13a96d65e101
Damien Schoof <damien.schoof@gmail.com>
2013-06-21 23:38:15 -0400

    Add system call to determine the number of processors.
    This allows us to dynamically determine the number of threads to spin
    off, and make optimal use of the cores available.

--- sorting/merge-sort-par.rs
+++ sorting/merge-sort-par.rs
@@ -8,11 +8,15 @@ use std::comm::{stream, Chan, Port};
 use std::cell::Cell;
 use benchmark::Benchmark;
 
+static _SC_NPROCESSORS_ONLN: i32 = 84;
+
 fn parallel_merge_sort_helper<T:Ord+Copy+Owned>(arr: ~[T]) -> ~[T] {
-    parallel_merge_sort(arr, 0)
+    let MAX_THREADS = unsafe {std::libc::funcs::posix88::unistd::sysconf(_SC_NPROCESSORS_ONLN) as uint};
+
+    parallel_merge_sort(arr, 0, MAX_THREADS)
 }
 
-fn parallel_merge_sort<T:Ord+Copy+Owned>(arr: ~[T], depth: uint) -> ~[T] {
+fn parallel_merge_sort<T:Ord+Copy+Owned>(arr: ~[T], depth: uint, max_threads: uint) -> ~[T] {
     let length = arr.len();
     if length <= 1 {
         return arr.to_owned();
@@ -22,21 +26,21 @@ fn parallel_merge_sort<T:Ord+Copy+Owned>(arr: ~[T], depth: uint) -> ~[T] {
     let mut left = vec::slice(arr, 0, middle).to_owned();
     let mut right = vec::slice(arr, middle, length).to_owned();
 
-    if depth < 8 {
+    if depth < max_threads {
         /* Create channel to pass the results back */
         let (port, chan): (Port<~[T]>, Chan<~[T]>) = stream();
         let left_cell = Cell::new(left); // the only way to access the above mutable field
         do spawn {
             // take the ref out of the cell, sort it, and send it back to the parent process
-            let sorted_left =  parallel_merge_sort(left_cell.take(), depth + 1);
+            let sorted_left =  parallel_merge_sort(left_cell.take(), depth + 1, max_threads);
             chan.send(sorted_left);
         }
-        right = parallel_merge_sort(right, depth + 1);
+        right = parallel_merge_sort(right, depth + 1, max_threads);
 
         left = port.recv();
     } else {
-        left = parallel_merge_sort(left, depth);
-        right = parallel_merge_sort(right, depth);
+        left = parallel_merge_sort(left, depth, max_threads);
+        right = parallel_merge_sort(right, depth, max_threads);
     }
 
     merge(left, right)
