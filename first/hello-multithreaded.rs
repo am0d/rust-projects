ad773d684773ebf4b89748c3b49b188ba1223474
Damien Schoof <damien.schoof@gmail.com>
2014-06-29 22:56:21 -0400

    Update for rust changes

--- first/hello-multithreaded.rs
+++ first/hello-multithreaded.rs
@@ -4,14 +4,14 @@ use std::comm::channel;
 fn main () {
     let (sender, receiver) = channel::<int>();
 
-    for child_number in range(0, 20) {
+    for child_number in range(0, 20i) {
         let child_sender = sender.clone();
         spawn(proc() {
             child_sender.send(child_number);
         });
     }
 
-    for _ in range(0, 20) {
+    for _ in range(0, 20i) {
         let received = receiver.recv();
         println!("Message received from child {}", received);
     }
